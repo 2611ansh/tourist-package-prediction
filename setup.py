e5482be948a5caacd8d330e5188b4faf589fc599
2611ansh <anshjabalpur@gmail.com>
2023-11-20 11:17:32 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="tourism_package_prediction",
+    version="0.0.1",
+    author="ansh",
+    author_email="anshjabalpur@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
